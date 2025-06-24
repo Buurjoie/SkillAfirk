cc1838e755e7a255a314323e9067d322b3c08660
Mandionelaye <mandionelay99@gmail.com>
2025-06-24 15:23:59 +0000

    last 1.2 commit

--- src/components/AdSense/AdBanner.jsx
+++ src/components/AdSense/AdBanner.jsx
@@ -1,25 +1,14 @@
-import React, { useEffect } from 'react';
+import AdSenses from "./AdSense";
+
 
 function AdBanner() {
-  useEffect(() => {
-    try {
-      (window.adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {
-      console.error("Erreur pub :", e);
-    }
-  }, []);
 
   return (
-    <>
-     <ins
-     className="adsbygoogle"
-     style={{ display: 'block' }}
-     data-ad-client="ca-pub-2510660773997180"
-     data-ad-slot="9625621261"
-     data-ad-format="auto"
-     data-full-width-responsive="true">
-     </ins>
-       </>
+    <AdSenses
+        slot="9625621261" 
+        format="auto" 
+        style={{ width: '100%', height: '90px' }}
+      />
   );
 }
 
